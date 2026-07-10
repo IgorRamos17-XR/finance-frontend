25be8746db20f96f1e72f98f165b5fb244a9af62
Igor Ramos17-XR <igordesouzaramos17@gmail.com>
2026-07-10 11:26:18 -0300

    chore: configura URL da API por variável de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8080/api", //http://localhost:8080/api
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://127.0.0.1:8080/api",
 });
 
 api.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
